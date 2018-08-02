84f5c545770035530fed462f621b948b6053ee0e
teustoscano <ssstoscano@gmail.com>
2018-08-02 13:01:01 -0300

    Minimum swipe for the card to go over screen or be set to default position

--- src/Deck.js
+++ src/Deck.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
-import { View, Animated, PanResponder } from 'react-native';
+import { View, Animated, PanResponder, Dimensions } from 'react-native';
+
+const SCREEN_WIDTH = Dimensions.get('window').width;
+const SWIPE_THRESHOLD = 0.40 * SCREEN_WIDTH;
 
 class Deck extends Component{
 	constructor(props){
@@ -10,17 +13,31 @@ class Deck extends Component{
 			onPanResponderMove: (event, gesture) => {
 				position.setValue({x:gesture.dx , y:gesture.dy})
 			},
-			onPanResponderRelease: () => {}
+			onPanResponderRelease: (event, gesture) => {
+				if(gesture.dx > SWIPE_THRESHOLD){
+					console.log('right');	
+				} else if (gesture.dx < -SWIPE_THRESHOLD){
+					console.log('left swipe');
+				} else {
+					this.resetPosition();
+				}
+			}
+			
 		});
 		this.state = { panResponder, position };
+	}
+
+
+	resetPosition(){
+		Animated.spring(this.state.position,{toValue: {x:0 , y:0}}).start();
 	}
 
 
 	getCardStyle(){
 		const {position} = this.state;
 		const rotate = position.x.interpolate({
-			inputRange:[-500,0,500],
-			outputRange:['-120deg', '0deg', '120deg']
+			inputRange:[-SCREEN_WIDTH * 1.5,0,SCREEN_WIDTH * 1.5],
+			outputRange:['-90deg', '0deg', '90deg']
 		});
 
 
